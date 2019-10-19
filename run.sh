8c48d5644d8f4661410667c830c800d023e138c8
elnazsaberiansari <elnaz.saberiansari@gmail.com>
2019-10-19 22:12:23 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cat $(pwd)/code/empiricalpvalue.R | R --slave --args $(pwd)/data/preprocessed-da
 
 cat $(pwd)/code/randomSignatures.R | R --slave --args $(pwd)/data/preprocessed-data.Rda $(pwd)/data/signatures.Rda $(pwd)/output
 
-cat $(pwd)/code/diffusion.R | R --slave --args $(pwd)/code/input/diffusion/genelist.txt $(pwd)/code/input/diffusion/genescore.txt $(pwd)/data/PPI.txt $(pwd)/output
+cat $(pwd)/code/diffusion.R | R --slave --args $(pwd)/genelist.txt $(pwd)/genescore.txt $(pwd)/PPI.txt $(pwd)/output
 
-cat $(pwd)/code/geneSubsetPvalue.R | R --slave --args $(pwd)/data/U133A_combat.h5 $(pwd)/code/input/geneSubsetPvalue/ACESGenesEntrezID.txt $(pwd)/code/input/geneSubsetPvalue/sigGenelist.txt $(pwd)/output
+cat $(pwd)/code/geneSubsetPvalue.R | R --slave --args $(pwd)/data/U133A_combat.h5 $(pwd)/ACESGenesEntrezID.txt $(pwd)/sigGenelist.txt $(pwd)/output
